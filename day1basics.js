e58417b2d804bbbe8489eac6e7bd69ecdc65c8e6
tikishabudin <tiki@chaosarchitects.com>
2020-04-15 21:41:31 +0300

    Day 1

---
+++ day1basics.js
@@ -0,0 +1,74 @@
+// var someVar = "This is a string";
+// alert(someVar);
+// someVar = 10;
+// alert(someVar);
+// someVar = 10.5;
+// alert(someVar);
+// someVar = new Object();
+// alert(someVar);
+
+var sampleStr = "This is Tiki's string";
+sampleStr = 'This is also Tiki\'s string';
+
+var num = 10;
+// alert("The value you specified is: " + num);
+
+var x=10, y=5;
+/*
+RELATIONAL OPERATORS:
+== / === Same as memory address/ Same as value
+!= / !== Not same as memory address / Not same as value
+< , <= , > , >=
+
+LOGICAL OPERATORS:
+&& And
+|| Or
+! Not
+*/
+// if(x != y && x > 100 || y <100)
+// {
+//     alert("TRUE");
+// }
+// else
+// {
+//     alert("FALSE");
+// }
+
+// var userInput = prompt("Enter something")
+
+// switch(userInput)
+// {
+//     case "Hello": alert("Greetings");
+//         break;
+//     case 'Bye': alert("Goodbye");
+//         break;
+//     case "What": alert("Whatever");
+//         break;
+//     default: alert("Invalid entry");
+// }
+
+/*
+while
+do while
+for
+*/
+
+var counter = 0;
+
+for(var i = 0; i < 3; i++,console.log("Let's do this"));
+// {
+//     console.log("Salam");
+//     counter++;
+// }
+
+// do{
+//     console.log("Moi");
+//     counter++;
+// }while(counter < 5);
+
+// while(counter < 5)
+// {
+//     console.log("Hello");
+//     counter ++;
+// }
+
